e1f3f4459b8213e7413dda48f890bc47ceca4605
Tamer Berat Çelik <aducat5@hotmail.com>
2022-09-20 10:06:15 +0300

    renamed test service to base service, renamed dapper context

--- ProjectBase.Data/Database/DapperContext.cs
+++ ProjectBase.Data/Database/DapperContext.cs
@@ -1,6 +1,6 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
 using System.Data;
-using System.Data.SqlClient;
 
 namespace ProjectBase.Data.Database
 {
---
+++ ProjectBase.Data/Services/BaseService.cs
@@ -0,0 +1,30 @@
+﻿using Dapper;
+using ProjectBase.Data.Database;
+
+namespace ProjectBase.Data.Services
+{
+    public interface IBaseService
+    {
+        public Task<IEnumerable<int>> GetNumber();
+    }
+    public class BaseService : IBaseService
+    {
+        private DapperContext _context;
+        public BaseService(DapperContext context)
+        {
+            //get db here
+            _context = context;
+        }
+
+        public async Task<IEnumerable<int>> GetNumber()
+        {
+            var query = @"
+                SELECT 1
+            ";
+
+            using var connection = _context.CreateConnection();
+            var tests = await connection.QueryAsync<int>(query);
+            return tests;
+        }
+    }
+}
