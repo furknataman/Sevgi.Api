d9ee63f5321c30c3daa7f8025ba4628c3d6a01cf
Tamer Berat Çelik <aducat5@hotmail.com>
2022-09-20 10:21:09 +0300

    added controllers and request response models

---
+++ ProjectBase.Api/Controllers/AuthController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using ProjectBase.Api.Infrastructure.RequestModels;
+using ProjectBase.Data.Services;
+
+namespace ProjectBase.Api.Controllers
+{
+    [ApiController]
+    [Route("auth")]
+    public class AuthController : Controller
+    {
+        private readonly IAuthenticationService _authService;
+ 
+        public AuthController(IAuthenticationService authService)
+        {
+            _authService = authService;
+        }
+
+
+        [AllowAnonymous]
+        [HttpPost("login")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(string))]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> Login([FromBody] LoginRequest request)
+        {
+            var response = await _authService.Login(request.Email, request.Password);
+
+            return Ok(response);
+        }
+
+        [AllowAnonymous]
+        [HttpPost("register")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(string))]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> Register([FromBody] RegisterRequest request)
+        {
+            var response = await _authService.Register(request.Email, request.Password);
+
+            return Ok(response);
+        }
+    }
+}
