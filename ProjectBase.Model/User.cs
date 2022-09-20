8dcd3c4123c967d259f5e94a0ac018085690a7f8
Tamer Berat Çelik <aducat5@hotmail.com>
2022-09-20 09:47:54 +0300

    added projects, dependencies and base user model

---
+++ ProjectBase.Model/User.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectBase.Model
+{
+    public class User : IdentityUser
+    {
+        //this is the basic user inherited from IdentityUser. you can modify as you want but make sure you update the database.
+    }
+}
