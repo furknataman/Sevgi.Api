142477ef86ed1a135fcc708c0ceb66c5277f5fe2
Tamer Berat Çelik <aducat5@hotmail.com>
2022-09-20 10:10:33 +0300

    added identity and jwt to program cs

--- ProjectBase.Api/Program.cs
+++ ProjectBase.Api/Program.cs
@@ -1,6 +1,61 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using ProjectBase.Data.Database;
+using ProjectBase.Model;
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+
+#region Identity
+
+//adding identity context
+builder.Services.AddDbContext<BaseIdentityContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("BaseProjectDb")));
+
+//adding indentity
+builder.Services
+    .AddIdentity<User, IdentityRole>()
+    .AddEntityFrameworkStores<BaseIdentityContext>()
+    .AddDefaultTokenProviders();
+
+builder.Services.Configure<IdentityOptions>(options =>
+{
+    //this is where you can configure anything about users and authorization
+    options.Password.RequireNonAlphanumeric = false;
+    //options.SignIn.RequireConfirmedEmail = false;
+    //options.SignIn.RequireConfirmedAccount = false;
+    //options.User.RequireUniqueEmail = false;
+});
+#endregion
+
+#region JWT
+
+//adding JWT
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+{
+    options.SaveToken = true;
+    options.RequireHttpsMetadata = false;
+    options.TokenValidationParameters = new TokenValidationParameters()
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidAudience = builder.Configuration["JWT:ValidAudience"],
+        ValidIssuer = builder.Configuration["JWT:ValidIssuer"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JWT:Secret"]))
+    };
+});
+
+#endregion
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
--- ProjectBase.Data/Database/IdentityContext.cs
+++ ProjectBase.Data/Database/IdentityContext.cs
@@ -4,9 +4,9 @@ using ProjectBase.Model;
 
 namespace ProjectBase.Data.Database
 {
-    public class BaseProjectIdentityContext : IdentityDbContext<User>
+    public class BaseIdentityContext : IdentityDbContext<User>
     {
-        public BaseProjectIdentityContext(DbContextOptions<BaseProjectIdentityContext> options) : base(options)
+        public BaseIdentityContext(DbContextOptions<BaseIdentityContext> options) : base(options)
         {
             //this is the dbcontext which creates and connects to your database for identity operations.
             //use options if you will have startup settings
