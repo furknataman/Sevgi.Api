84a767b3cd4c9f44f4a8af8859050853f5689489
Tamer Berat Çelik <aducat5@hotmail.com>
2022-09-20 10:15:43 +0300

    updated program cs for injecting

--- ProjectBase.Api/Program.cs
+++ ProjectBase.Api/Program.cs
@@ -2,7 +2,9 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using ProjectBase.Data.Database;
+using ProjectBase.Data.Services;
 using ProjectBase.Model;
 using System.Text;
 
@@ -57,10 +59,63 @@ builder.Services.AddAuthentication(options =>
 #endregion
 
 
+//adding db context as singleton
+builder.Services.AddSingleton<DapperContext>();
+
+//adding authservice
+builder.Services.AddScoped<IAuthenticationService, AuthenticationService>();
+
+//adding repository services
+////adding test repo
+builder.Services.AddScoped<IBaseService, BaseService>();
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Base TEST API", Version = "v1" });
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Description = @"JWT Authorization header using the Bearer scheme. \r\n\r\n 
+                      Enter 'Bearer' [space] and then your token in the text input below.
+                      \r\n\r\nExample: 'Bearer 12345abcdef'",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer"
+    });
+
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement()
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                },
+                Scheme = "oauth2",
+                Name = "Bearer",
+                In = ParameterLocation.Header,
+
+            },
+            new List<string>()
+        }
+    });
+});
+
+//adding CORS policy for dev client
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAngularDevClient", b =>
+    {
+        b.WithOrigins("http://localhost:4200")
+        .AllowAnyHeader()
+        .AllowAnyMethod();
+    });
+});
 
 var app = builder.Build();
 
@@ -72,6 +127,9 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+//use authentication. this is for protecting controlllers with auth service and identity
+app.UseAuthentication();
 
 app.UseAuthorization();
 
