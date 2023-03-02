5f4073798eee16c3d033ca3e203a9d8f05e37a5f
Furkan ATAMAN <furknataman@gmail.com>
2023-03-02 23:16:24 +0300

    added update create

---
+++ Sevgi.Model/File.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace Sevgi.Model
+{
+	public class Store
+	{
+		public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Percentage { get; set; }
+        public int FileId { get; set; }
+        public string ExternalId { get; set; }
+        public bool IsDeleted { get; set; }
+
+    }
+}
+
