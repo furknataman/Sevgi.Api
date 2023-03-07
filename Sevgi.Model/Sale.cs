c1f253da974d00b786345f8c826e57f088cf1a10
Furkan ATAMAN <furknataman@gmail.com>
2023-03-07 23:49:39 +0300

    added file upload

---
+++ Sevgi.Model/Sale.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sevgi.Model
+{
+    public class User : IdentityUser
+    {
+        //this is the basic user inherited from IdentityUser.
+        //you can modify as you want but make sure you update the database.
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public Genders Gender { get; set; }
+        public DateTime BirthDate { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public bool IsActive { get; set; }
+
+    }
+}
---
+++ Sevgi.Model/UserUpdateResponse.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Sevgi.Model
+{
+    public class AuthResponse
+    {
+        public string Token { get; set; } = string.Empty;
+        public bool IsRegistered { get; set; }
+    }
+}
