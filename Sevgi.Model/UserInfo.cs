476cf9f2a4a1080df7f1de7793aa5bf2d24e1ad2
Furkan ATAMAN <furknataman@gmail.com>
2023-03-20 15:34:49 +0300

    added userinfo

---
+++ Sevgi.Model/UserInfo.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sevgi.Model
+{
+    public class UserView 
+    {
+        public string Id { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string PhoneNumber { get; set; } = string.Empty;
+        public Genders Gender { get; set; }
+        public DateTime BirthDate { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public bool IsActive { get; set; }
+        public double TotalAmount { get; set; }
+        public double Bonus { get; set; }
+        public int FileId { get; set; }
+
+
+    }
+}
---
+++ Sevgi.Model/UserSale.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sevgi.Model
+{
+    public class Sale
+    {
+       
+        public int SaleId { get; set; } 
+        public string BranchCode { get; set; } = string.Empty;
+        public string CashCode { get; set; } = string.Empty;
+        public string CashierCode { get; set; } = string.Empty;
+        public string CardNo { get; set; } = string.Empty;
+        public double Total { get; set; } 
+        public DateTime AddDate { get; set; } 
+        public bool IsReturn { get; set; }
+        public double CardTotal { get; set; }
+
+
+
+    }
+}
