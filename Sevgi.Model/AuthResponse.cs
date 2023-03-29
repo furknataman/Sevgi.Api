a0e4e5cfe3350668b42a4b6345cabd920b9e1627
Tamer Berat Çelik <aducat5@hotmail.com>
2023-03-29 12:17:19 +0300

    Update AuthResponse.cs

--- Sevgi.Model/AuthResponse.cs
+++ Sevgi.Model/AuthResponse.cs
@@ -13,7 +13,7 @@ namespace Sevgi.Model
         {
             Message = message;
         }
-        public AuthResponse(string message, isBanned)
+        public AuthResponse(string message, bool isBanned)
         {
             Message = message;
             IsBanned = isBanned;
@@ -21,6 +21,7 @@ namespace Sevgi.Model
         public string Token { get; set; } = string.Empty;
         public bool IsSuccessful { get; set; }
         public bool IsUserReady { get; set; }
+        public bool IsBanned { get; set; }
         public string Message { get; set; } = string.Empty;
     }
 }
